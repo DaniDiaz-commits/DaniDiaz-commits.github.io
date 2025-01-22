56cf696645b814bc60d38f6041c2ba07e7b7e625
Daniel Diaz <147053568+DaniDiaz-commits@users.noreply.github.com>
2025-01-22 19:52:57 +0100

    Adicción de notificaciones Y hacer que el botón del de la descarga funcione correctamente (quiza haya bugs)

--- js/script.js
+++ js/script.js
@@ -7,16 +7,48 @@ var typed = new Typed(".typing", {
 });
 
 const download = document.querySelector("#download");
+const noti = document.querySelector("#notificacion");
+
+// Para que no se recarge la página.
 download.addEventListener("click", (event) => {
     event.preventDefault();
 
     // const userChoice = confirm("¿Quieres descargar el CV en inglés? (Aceptar = Español, Cancelar = Inglés)");
-    const userChoice = prompt("¿Quieres descargar el CV en inglés? (es = Español, en = Inglés)").trim().toLowerCase();
+    // const userChoice = prompt("¿Quieres descargar el CV en inglés? (es = Español, en = Inglés)");
     
-    if(userChoice === "es") window.location.href = "files/Curriculum en español.pdf";
-    else if (userChoice === "en")  window.location.href = "files/Curriculum en inglés.pdf";
-    else alert("Descarga cancelada.");
+    // if(userChoice === "es") window.location.href = "files/CV-DanielDiaz-Español.pdf";
+    // else if (userChoice === "en")  window.location.href = "files/CV-DanielDiaz-English.pdf";
+    // else {
+    //     // alert("Descarga cancelada.");
+    //     noti.style.display = "block";
+    //     // download.disabled = true;
+    //     download.setAttribute("disabled", "");
+    //     setTimeout(() => {
+    //         noti.style.display = "none";
+    //         download.removeAttribute("disabled");
+    //         // download.disabled = false;
+    //     }, 3000);
+    // }
 });
+
+function descarga(event) {
+    event.preventDefault();
+
+    // const userChoice = confirm("¿Quieres descargar el CV en inglés? (Aceptar = Español, Cancelar = Inglés)");
+    const userChoice = prompt("¿Quieres descargar el CV en inglés? (es = Español, en = Inglés)");
+    
+    if(userChoice === "es") window.location.href = "files/CV-DanielDiaz-Español.pdf";
+    else if (userChoice === "en")  window.location.href = "files/CV-DanielDiaz-English.pdf";
+    else {
+        // alert("Descarga cancelada.");
+        noti.style.display = "block";
+        download.removeAttribute("onclick");
+        setTimeout(() => {
+            noti.style.display = "none";
+            download.setAttribute("onclick", "descarga(event)");
+        }, 3000);
+    }
+}
 
 const proyects = document.querySelector("#proyects");
 proyects.addEventListener("click", (e) => {
@@ -24,3 +56,4 @@ proyects.addEventListener("click", (e) => {
     if(e.target.id == "weed") { window.location.href = "proyects/weed/index.html"; }
     if(e.target.id == "ip") { window.location.href = "proyects/ip/index.html"; }
 })
+
