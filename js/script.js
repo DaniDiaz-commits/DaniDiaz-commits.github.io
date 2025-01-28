3089033f9846838fe5a5e33394e4210678e600c8
Daniel Diaz <147053568+DaniDiaz-commits@users.noreply.github.com>
2025-01-29 00:32:21 +0100

    Adiccion de dos proyectos nuevos. Uno de ellos no es lo suficientemente "proyecto" como para añadirlo al portfolio asique se queda fuera

--- js/script.js
+++ js/script.js
@@ -49,12 +49,3 @@ function descarga(event) {
         }, 3000);
     }
 }
-
-// const proyects = document.querySelector("#proyects");
-// proyects.addEventListener("click", (e) => {
-//     if(e.target.id == "horario") { window.location.href = "proyects/horario/index.html"; }
-//     if(e.target.id == "weed") { window.location.href = "proyects/weed/index.html"; }
-//     if(e.target.id == "ip") { window.location.href = "proyects/ip/index.html"; }
-//     if(e.target.id == "drum") { window.location.href = "proyects/drum-kit/index.html"; }
-// })
-
